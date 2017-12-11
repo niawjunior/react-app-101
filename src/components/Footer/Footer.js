fb358acf01d8a73ca76a0758c11f692da45529e8
niawjunior <pasupol@dome.cloud>
2017-12-11 17:56:09 +0700

    :tada:

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Footer = () => {
+        return (
+          <div className="App">
+           my Footer
+          </div>
+        );
+}
+
+export default Footer;
