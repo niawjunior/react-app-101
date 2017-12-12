3bc3bdcfc75f3af61273cdf4534e709cd1bf6bf6
niawjunior <pasupol@dome.cloud>
2017-12-12 11:49:46 +0700

    get data from api

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
-
+import './Faq.css';
+import axios from 'axios';
 class Faq extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            faqs: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                const faqs = res.data.slice(0,10);
+                this.setState({ faqs });
+            })
+    }
+
     render() {
         return (
             <div>
-                Faq
+            <div class="container">
+            <section class="section">
+              <h1 class="title">FAQ</h1>
+              <h2  class="subtitle is-4">Lorum ipsum and all  of that jazz.</h2>
+      
+              <div style={{ marginTop:'10px'}} class="columns">
+                {this.state.faqs.map(faq =>
+                    <div class="column is-one-third">
+                    <div class="card">
+                      <div class="card-content">
+                        <p class="title">{ faq.title }</p>
+                        <p class="answer">{ faq.body }</p>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </section>
+          </div>
             </div>
         );
     }
